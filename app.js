b41f2a87c8f915028de0079b2a0cb2acec70b194
FullyStackedCoder <kvn.praveen27@gmail.com>
2019-02-06 18:49:35 +0530

    Added Passport Local Config

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const socket = require('socket.io');
+
+const LocalStrategy = require('passport-local').Strategy;
 
 const config = require('./config');
 
@@ -12,6 +15,17 @@ const io = socket(app.server);
 app.use(bodyParser.json({
   limit: config.bodyLimit
 }));
+
+app.use(passport.initialize());
+let Account = require('./models/account');
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+},
+  Account.authenticate()
+));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.get('/', (req, res, next) => {
   res.json({message: 'Chat API is ALIVE!'})
