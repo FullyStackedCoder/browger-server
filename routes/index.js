fb2e117b741d6e3345a337933789ddf156d6198c
FullyStackedCoder <kvn.praveen27@gmail.com>
2019-02-20 17:03:22 +0530

    Added user related routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const config = require('../config');
 const middleware = require('../middleware');
 const initDb = require('../util/database');
 const account = require('../controllers/account');
+const user = require('../controllers/user');
 
 let router = express();
 
 initDb(database => {
   router.use(middleware({config, database}));
 
-  router.use('/account', account({config, database}));
+  router.use('/user', user({ config, database }));
+  router.use('/account', account({ config, database }));
 });
 
 module.exports = router;
